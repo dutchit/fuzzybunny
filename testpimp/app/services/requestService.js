bbb89a18868b53f49c95fc61a7a88c92cd174135
elisa4923 <elisa@Elisas-MacBook-Pro-2.local>
2015-09-13 16:22:34 -0700

    more stuff
    Click log in should show “Registration Form Is Not Valid” from
    developer’s tool view

--- testpimp/app/services/requestService.js
+++ testpimp/app/services/requestService.js
@@ -1,14 +1,17 @@
 angular.module('testpimp').factory('requestService',['$http', 'RESTfulAPI', function($http, RESTfulAPI){
   
 	var service = {};
-  
+	var somedata = {'username': 'rhonrado', 'password': 'aaaaaa', 'displayName': 'Ryan Honrado'};
+	var mytestdata = {"username":"unique", "password": "password", "displayName":"You Nique"};
 
 	service.test = function() {
 		var response = $http({
-			url : RESTfulAPI.test(),
+			url : 'http://localhost:8000/appAdmin/',
 			method : 'POST',
-			data: {"username":"unique", "password": "password", "displayName":"You Nique"},
-			dataType: 'json'
+			headers: {
+		        'Content-type': 'application/json'
+		    },
+			data: somedata,
 				}).success(function(data, status, headers, config) {
 					return data;
 				}).error(function(data, status, headers, config) {
