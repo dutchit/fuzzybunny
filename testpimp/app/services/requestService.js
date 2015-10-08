fefed6e08aa446bb0f110ec56cfc9b1869bfa765
xephos049 <joseph.joe93@gmail.com>
2015-10-07 22:54:25 -0700

    Revert "attempted to add getJobList function"
    This reverts commit a779ba7fdda4f4c82c5de9fcb7745dcdf81f52f3.
    
    Conflicts:
    	testpimp/app/services/requestService.js

--- testpimp/app/services/requestService.js
+++ testpimp/app/services/requestService.js
@@ -57,6 +57,7 @@ service.getMyProviderProfiles = function(userId) {
 	//TODO
 
 	service.requestForJobs = function() {
+<<<<<<< HEAD
 		service.register = function(postPayload) {
 			console.log("postPayload: " + JSON.stringify(postPayload));
 			var response = $http({
@@ -73,63 +74,54 @@ service.getMyProviderProfiles = function(userId) {
 				return data;
 			});
 			return response;
+=======
+=======
+	service.register = function(postPayload) {
+		console.log("postPayload: " + JSON.stringify(postPayload));
+		var response = $http({
+			url : restServer + 'api/userprofiles/',
+			method : 'POST',
+			headers: {
+		        'Content-type': 'application/json'
+		    },
+			data: postPayload,
+				}).success(function(data, status, headers, config) {
+					return data;
+				}).error(function(data, status, headers, config) {
+					console.log("reg api post failed.")
+					return data;
+				});
+				return response;
+>>>>>>> parent of a779ba7... attempted to add getJobList function
 		};
-
-		service.login = function (credential) {
-			return 'sure';
-		}
-		return service;
+  
+	service.login = function (credential) {
+		return 'sure';
 	}
 
-	service.getJobList = function(id) {
-		// Future Param: category
+	service.getMyProviderProfiles = function() {
+//		console.log("postPayload: " + JSON.stringify(postPayload));
+>>>>>>> master
+		var response = $http({
+			url : restServer + 'api',
+			method : 'GET',
+			headers: {
 
-		var job = getConstants.provideJobs;
+			},
+		}).success(function(table){
+			return 
+		}).error(function () {
+			return response
+		})
+};
 
-		var myTableDiv = document.getElementById("table")
-		var table = document.createElement('TABLE')
-		var tableBody = document.createElement('TBODY')
+return service;
 
-		table.border = '1'
-		table.appendChild(tableBody);
-
-		var heading = new Array(); // columns
-        heading[0] = "Requests"
-
-        var stock = new Array() // rows
-        // stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
-        // stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
-        // stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
-        stock[0] = job.provider
-        stock[1] = job.title
-        stock[2] = job.description
-        stock[3] = job.location
-        stock[4] = job.price
-
-        //TABLE COLUMNS
-        var tr = document.createElement('TR');
-        tableBody.appendChild(tr);
-        for (i = 0; i < heading.length; i++) {
-          	var th = document.createElement('TH')
-            th.width = '75';
-            th.appendChild(document.createTextNode(heading[i]));
-            tr.appendChild(th);
-
-        }
-
-        //TABLE ROWS
-        var tr = document.createElement('TR');
-        tableBody.appendChild(tr);
-
-        for (i = 0; i < stock.length; i++) {
-          	for (j = 0; j < stock[i].length; j++) {
-           		var td = document.createElement('TD')
-           		td.appendChild(document.createTextNode(stock[i][j]));
-           		td.appendChild(td)
-           	}
-        }
-
+<<<<<<< HEAD
         myTableDiv.appendChild(table)}
 	}
 	]
 );
+=======
+}]);
+>>>>>>> parent of a779ba7... attempted to add getJobList function
