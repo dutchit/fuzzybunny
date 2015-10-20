d7088f5721c211c838d76f60caab681c078766fd
PlzNoticeMeSenpai <mramos2692@gmail.com>
2015-10-20 16:43:36 -0700

    updated searchForJobCtrl_M and the html to have all jobs by default and then filter by category. Remember to remove _M in some of the code when copying and pasting.

--- testpimp/app/js/searchForJobCtrl_M.js
+++ testpimp/app/js/searchForJobCtrl_M.js
@@ -4,8 +4,14 @@ angular.module('testpimp').controller('searchForJobCtrl_M', function ($rootScope
 
 requestService_M.getCategories().then(
             function(success) {
-                
-                $scope.jobCategories = success.data; 
+                var cats = [];
+                cats.push("All Jobs");
+                for(var objs in success.data){
+                    console.log(success.data[objs]);
+
+                    cats.push(success.data[objs]);
+                }
+                $scope.jobCategories = cats; 
                 console.log("Cats:" + success.data);
 
 
@@ -15,20 +21,33 @@ requestService_M.getCategories().then(
             }
     );
 
+requestService_M.getAllJobs().then(
+
+    function(success) {
+        $scope.jobs = success.data;
+    },
+    function(error) {
+
+    }
+
+    );
+
 $scope.changeNuggetJobs = function() {        
         requestService_M.getAllJobs().then(
             function(success) {
-                $scope.jobs = []
-                for(var obj in success.data){
-                   if (success.data[obj]["category"] == $scope.jobCats) {
-                    $scope.jobs.push(success.data[obj])
-                   } 
-                 }
-//                 if ($scope.jobCats ==  success.data["category"]) {
-//                     $scope.jobs = success.data;
-//                 }
-                
-// //              console.log("jobs: " + success.data);
+
+                if($scope.jobCats == "All Jobs") {
+                    $scope.jobs = success.data;
+                } 
+                else {
+                     $scope.jobs = [];                
+                     for(var obj in success.data){                   
+                        if (success.data[obj]["category"] == $scope.jobCats) {                    
+                            $scope.jobs.push(success.data[obj])                   
+                        }                 
+                    }
+                }
+               
             },
              function(error){
 
