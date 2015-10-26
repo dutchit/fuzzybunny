d7253fb3f67954893a8cb5af7a6a51583018645b
elisa4923 <elisa@Elisas-MacBook-Pro-2.local>
2015-10-26 14:48:42 -0700

    merge

--- testpimp/app/js/job/myPostedJobsCtrl.js
+++ testpimp/app/js/job/myPostedJobsCtrl.js
@@ -2,16 +2,9 @@ angular.module('testpimp').controller('myPostedJobsCtrl', function ($rootScope, 
 	
 	requestService.getMyPostings($scope.user.id).then(
 			function(success) {
-				$scope.myPostings = success.data;
-				console.log("jobs: " + success.data);
-//				shareDataService.setProviderProfiles(myProviderProfiles);
-/*                    if ($scope.newjob.price.option == $scope.paymentOptions[0].option) {
-		            $scope.newjob.compensation = "$" + $scope.newjob.setPrice;
-	            } else if ($scope.newjob.price.option == $scope.paymentOptions[1].option) {
-		            $scope.newjob.compensation = "From $" + $scope.newjob.lowerbound + " to $" + $scope.newjob.upperbound;
-	            }
-				$scope.contentUrl = 'partials/myProfiles.html';
-*/				},
+				$scope.myPostedJobs = success.data;
+				console.log("jobs: " + JSON.stringify(success.data));
+			},
 		     function(error){
 
 		    }
