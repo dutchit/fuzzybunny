75081f901494ad842e524646c1e41b630066bd0b
xephos049 <joseph.joe93@gmail.com>
2015-10-25 18:46:02 -0700

    failed get applicants (Elisa beat me)

--- testpimp/app/js/getApplicantsCtrl.js
+++ testpimp/app/js/getApplicantsCtrl.js
@@ -1,8 +1,26 @@
 angular.module('testpimp').controller('getApplicantsCtrl', function ($rootScope, $scope,shareDataService, requestService, requestService_Joe) {
 
-	requestService_Joe.getApplicants().then(
+	// requestService_Joe.getApplicants($scope.jobID).then(
+	// 		function(success) {
+	// 			$scope.applicants = success.data;
+	// 			console.log(success.data);
+	// 			// for (var i in success.data){
+	// 			// 	$scope.applicants.push(requestService_Joe.getApplicationHelper(success.data[i][applicantID], success.data[i][providerID]));
+	// 			// }
+	// 			// $scope.applicants = success.data;
+	// 		}, 
+	// 		function(error){
+	// 			console.log(error);
+	// 		}
+	// 	);
+
+	$scope.goToProfile = function(applicant){
+		// go to app.userID
+	}
+
+	//$scope.getApplicantProfiles = function(){
+		requestService_Joe.getApplicants($scope.jobID).then(
 			function(success) {
-				console.log(success.data);
 				$scope.applicants = success.data;
 			}, 
 			function(error){
@@ -10,9 +28,18 @@ angular.module('testpimp').controller('getApplicantsCtrl', function ($rootScope,
 			}
 		);
 
-	$scope.goToProfile = function(applicant){
-		// go to app.userID
-	}
+		$scope.applicantProfiles = [];
+		for (var i in $scope.applicants){
+			requestService_Joe.getApplicationHelper($scope.applicants[i].applicantID, $scope.applicants[i].providerID).then(
+				function(success){
+					$scope.applicantProfiles.push(success.data);
+				},
+				function(error){
+					console.log(error);
+				});
+		}
+	//}
+
 
 	$scope.showDescription = false;
 
--- testpimp/app/services/requestService_Joe.js
+++ testpimp/app/services/requestService_Joe.js
@@ -23,17 +23,36 @@ var restServer = 'http://rhonrado.pythonanywhere.com/';
 	};
 
 	service.getApplicants = function(id){ 
+		var applicants = [];
 		var response = $http({ 
-		// Add'/applicants' if possible to make job easier 
 		url : restServer +'api/jobs/' + id + '/applicants', 
 		method : 'GET', 
 		headers: {'Content-type': 'application/json'     
 	}, }).success(function(data,status, headers, config) { 
-		return data; 
+		// for (var i = 0; i < data.length; i++)
+		// {
+		// 	applicants.push(service.getApplicantsHelper(data[i].applicantID, data[i].providerprofileID));
+		// }
+		// console.log(applicants);		
+		// console.log(applicants[0].$$state);
+		// return applicants;
+		return data;
 	}).error(function(data, status,headers, config) { 
 		return data; 
 	}); 
 	return response; }; 	
+
+	service.getApplicantsHelper = function(applicantID, providerID){
+		var response = $http({ 
+		url : restServer +'api/providerprofiles/' + applicantID + '/' + providerID, 
+		method : 'GET', 
+		headers: {'Content-type': 'application/json'     
+	}, }).success(function(data,status, headers, config) { 
+		return data[0];
+	}).error(function(data, status,headers, config) { 
+		return data; 
+	}); 
+	return response; }; 
 	
 	return service;
 
